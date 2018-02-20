c12a7f4e7e606a233bcf8536a2fccc0c2a029d95
adam523 <25071854@qq.com>
2018-02-19 16:00:24 -0800

    limiting amplitude wave filter
    LAF

---
+++ digitalFilter/LAF.c
@@ -0,0 +1,11 @@
+% limiting amplitude wave filter
+#define A 10 // can be set by need
+char value;  // for 8 bit ADC only, can be set as 'bit'
+char laf()
+{
+        char new_value;
+        new_value = get_ad();
+        if ( ( new_value - value > A ) || ( value - new_value > A )
+        return value;
+        return new_value;
+}
