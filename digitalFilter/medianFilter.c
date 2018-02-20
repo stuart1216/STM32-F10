aa7094b8bae0e27204319d5f5b6e2075615ee4d2
adam523 <25071854@qq.com>
2018-02-19 16:05:17 -0800

    bubble sort
    using buble sort algorithm, can be replaced by other more efficent algorithm

--- digitalFilter/medianFilter.c
+++ digitalFilter/medianFilter.c
@@ -3,24 +3,20 @@
 #define N 11
 char filter()
 {
-        char value_buf[N];
-        char count,i,j,temp;
-        for ( count=0;count<N;count++)
-        {
-                value_buf[count] = get_ad();
-                delay();
+    char value_buf[N];
+    char count,i,j,temp;
+    for ( count=0;count<N;count++)    {
+        value_buf[count] = get_ad();
+           delay();
+    }
+    for (j=0;j<N-1;j++)  {
+        for (i=0;i<N-j;i++) {
+            if ( value_buf>value_buf[i+1] ) {
+                temp = value_buf;
+                value_buf = value_buf[i+1]; 
+                value_buf[i+1] = temp;
+            }
         }
-        for (j=0;j<N-1;j++)
-        {
-                for (i=0;i<N-j;i++)
-                {
-                        if ( value_buf>value_buf[i+1] )
-                        {
-                                temp = value_buf;
-                                value_buf = value_buf[i+1]; 
-                                value_buf[i+1] = temp;
-                        }
-                }
-        }
-return value_buf[(N-1)/2];
+    }
+    return value_buf[(N-1)/2];
 } 
